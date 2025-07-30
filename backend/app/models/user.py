f15b1aa706875c09b060b9a060bcc324cd9992d4
-m <villamorjohnnoel@gmail.com>
2025-07-31 01:23:54 +0800

    feat(be): create user model.

---
+++ backend/app/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+from app.db.session import Base
+import uuid
+
+class User(Base):
+  __tablename__ = "users"
+  
+  id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+  email = Column(String, unique=True, nullable=False)
+  first_name = Column(String, nullable=False)
+  last_name = Column(String, nullable=False)
+  hashed_password = Column(String, nullable=False)
+  created_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
+  updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now(), nullable=False)
+
+  def __repr__(self):
+    return f"<User(id={self.id}, email={self.email}, first_name={self.first_name}, last_name={self.last_name})>"
